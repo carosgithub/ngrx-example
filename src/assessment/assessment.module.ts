5f4b42ab5560d0210dc3002d769f6631ac6db3c0
OscarNarvaez <oscar.l.narvaez-jimbo@dhsoha.state.or.us>
2020-10-15 17:00:12 -0700

    commit

--- src/assessment/assessment.module.ts
+++ src/assessment/assessment.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
-import { AssessmentTagComponent } from "./component/assessment.component";
+import { AssessmentTagComponent } from "./container/assessment.component";
 import { TodoModule } from './todo/todo.module';
 
 @NgModule({
